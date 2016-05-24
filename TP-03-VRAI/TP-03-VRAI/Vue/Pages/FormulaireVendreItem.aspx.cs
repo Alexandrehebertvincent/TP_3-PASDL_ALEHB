a50a59c78e4e99e892c92a1dfebbc84ab3a3f08d
Pasqual24 <Pasqual24@live.ca>
2016-05-24 16:50:11 -0400

    ptit push

--- TP-03-VRAI/TP-03-VRAI/Vue/Pages/FormulaireVendreItem.aspx.cs
+++ TP-03-VRAI/TP-03-VRAI/Vue/Pages/FormulaireVendreItem.aspx.cs
@@ -12,6 +12,36 @@ namespace TP_03_VRAI.Vue.Pages
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            Label lblTitreItem = new Label();
+            lblTitreItem.Text = "Titre de l'item";
+            Label lblDescription = new Label();
+            lblDescription.Text = "Description";
+            Label lblPrix = new Label();
+            lblPrix.Text = "Prix plancher:";
+            Button btnPosterItem = new Button();
+            btnPosterItem.Text = "Publier l'annonce";
+
+            //Input
+            TextBox txtTitre = new TextBox();
+            TextBox txtDescription = new TextBox();
+            TextBox txtPrixPlancher = new TextBox();
+            Image image = new Image();
+
+            //DESIGN / ESTHÉTIQUE
+            txtTitre.Width = 300;
+            txtDescription.Width = 600;
+            txtDescription.Height = 200;
+            txtPrixPlancher.Width = 80;
+
+            this.formAjoutItem.Controls.Add(lblTitreItem);
+            this.formAjoutItem.Controls.Add(txtTitre);
+            this.formAjoutItem.Controls.Add(lblDescription);
+            this.formAjoutItem.Controls.Add(txtDescription);
+            this.formAjoutItem.Controls.Add(image);
+            this.formAjoutItem.Controls.Add(lblPrix);
+            this.formAjoutItem.Controls.Add(txtPrixPlancher);
+            this.formAjoutItem.Controls.Add(btnPosterItem);
+
         }
     }
 }
